8c031bc4223e8b507a9ee5edac0a99ae66061522
Serban Andrei <usadekall@gmail.com>
2021-09-11 15:42:09 +0300

    Rewritting whole backend

--- backend/v1/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/accounts', require('./accounts.js'));
-router.use('/passwords', require('./passwords.js'));
-
-module.exports = router;
